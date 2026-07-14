ce2ec9eab236a2789ab1b65ec7dcd65e6de7886b
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-07-14 06:50:26 -0300

    feat: adicionar estrutura inicial do ScreenSound
    Cria a solução com projetos de API, console, dados e modelos compartilhados. Adiciona configuração inicial, entidades Artista e Musica, contexto Entity Framework, migrations e menus principais da aplicação.

---
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco;
+public class ScreenSoundContext: DbContext
+{
+    public DbSet<Artista> Artistas { get; set; }
+    public DbSet<Musica> Musicas { get; set; }
+
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .UseLazyLoadingProxies(); 
+    }
+}
---
+++ ScreenSound/Menus/MenuSair.cs
@@ -0,0 +1,12 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+public class MenuSair : Menu
+{
+    public override void Executar(DAL<Artista> artistaDAL)
+    {
+        Console.WriteLine("Tchau tchau :)");
+    }
+}
